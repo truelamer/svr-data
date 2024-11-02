92ebe5a64249dbdc7701014aeff0c85b33481bce
Ilya.F <ilja.filin2010@yandex.ru>
2024-11-02 11:31:38 +0300

    Сборка пакеты. Сиды

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Svr\Data\Seeders;
 
+use Database\Seeders\DataCompaniesLocationsSeeder;
+use Database\Seeders\DataCompaniesObjectsSeeder;
+use Database\Seeders\DataCompaniesSeeder;
 use Illuminate\Database\Seeder;
 
 class DataSeeder extends Seeder
 {
     public function run()
     {
-        (new DataAnimalsSeeder())->run();
+        (new DataCompaniesSeeder())->run();
+        (new DataCompaniesLocationsSeeder())->run();
+        (new DataCompaniesObjectsSeeder())->run();
     }
 }
