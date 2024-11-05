78bae5983a4611a547340991d4a9410c301e45be
Ilya.F <ilja.filin2010@yandex.ru>
2024-11-05 13:56:03 +0300

    Сборка пакеты. Сиды

--- database/migrations/2024_09_11_161818_create_svr_table_data_applications_animals.php
+++ database/migrations/2024_09_11_161818_create_svr_table_data_applications_animals.php
@@ -25,40 +25,25 @@ return new class extends Migration
                 $table->increments('application_animal_id')->comment('Инкремент');
                 $table->integer('application_id')->nullable(false)->default(null)->comment('id заявки');
                 $table->integer('animal_id')->nullable(false)->default(null)->comment('id животного');
-                $table->timestamp('application_animal_date_add')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))
-                    ->comment('дата добавления');
-                $table->timestamp('application_animal_date_sent')->nullable(true)->default(null)
-                    ->comment('Дата нажатия кнопки отправки животного на регистрацию');
-                $table->timestamp('application_animal_date_horriot')->nullable(true)->default(null)->comment(
-                    'дата отправки в хорриот'
-                );
-                $table->timestamp('application_animal_date_response')->nullable(true)->default(null)->comment(
-                    'дата получения ответа от хорриот'
-                );
-                $table->addColumn('system.application_animal_status', 'application_animal_status')->index()->nullable(false)->default('added')
-                    ->comment("статус животного ('added', 'deleted', 'sent', 'registered', 'rejected', 'finished'"
-                );
-                $table->timestamp('application_animal_created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))
-                    ->comment('дата создания записи'
-                    );
-                $table->timestamp('application_animal_date_last_update')->index()->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))
-                    ->comment('Дата последнего запроса к хорриоту'
-                    );
-                $table->string('application_herriot_application_id', 64)->nullable(true)->default(null)
-                    ->comment('application_id из хорриота'
-                );
-                $table->string('application_herriot_send_text_error', 1000)->nullable(true)->default(null)
-                    ->comment('Текст ошибки при отправке в Хорриот'
-                );
-                $table->string('application_herriot_check_text_error', 1000)->nullable(true)->default(null)
-                    ->comment('Текст ошибки при проверке статуса регистрации в Хорриот'
-                );
-                $table->timestamp('update_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->comment(
-                    'дата обновления записи'
-                );
+                $table->timestamp('application_animal_date_add')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->comment('дата добавления');
+                $table->timestamp('application_animal_date_sent')->nullable(true)->default(null)->comment('Дата нажатия кнопки отправки животного на регистрацию');
+                $table->timestamp('application_animal_date_horriot')->nullable(true)->default(null)->comment('дата отправки в хорриот');
+                $table->timestamp('application_animal_date_response')->nullable(true)->default(null)->comment('дата получения ответа от хорриот');
+                $table->addColumn('system.application_animal_status', 'application_animal_status')->index()->nullable(false)->default('added')->comment("статус животного ('added', 'deleted', 'sent', 'registered', 'rejected', 'finished'");
+                $table->timestamp('application_animal_created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->comment('дата создания записи');
+                $table->timestamp('application_animal_date_last_update')->index()->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Дата последнего запроса к хорриоту');
+                $table->addColumn('system.herriot_error_types', 'application_response_herriot_error_type')->nullable(false)->default('unknown')->comment("статус заявки ('created', 'prepared', 'sent', 'complete_full', 'complete_partial', 'finished')");
+                $table->string('application_response_herriot_error_code', 64)->nullable(true)->default(null)->comment('Код ошибки при отправке в Хорриот');
+                $table->addColumn('system.herriot_error_types', 'application_response_application_herriot_error_type')->nullable(false)->default('unknown')->comment("статус заявки ('created', 'prepared', 'sent', 'complete_full', 'complete_partial', 'finished')");
+                $table->string('application_response_application_herriot_error_code', 64)->nullable(true)->default(null)->comment('Код ошибки при ответе из Хорриот');
+                $table->string('application_herriot_application_id', 64)->nullable(true)->default(null)->comment('application_id из хорриота');
+                $table->string('application_herriot_send_text_error', 1000)->nullable(true)->default(null)->comment('Текст ошибки при отправке в Хорриот');
+                $table->string('application_herriot_check_text_error', 1000)->nullable(true)->default(null)->comment('Текст ошибки при проверке статуса регистрации в Хорриот');
+                $table->timestamp('update_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->comment('дата обновления записи');
             });
         }
     }
+
 
     /**
      * Reverse the migrations.
--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -2,17 +2,28 @@
 
 namespace Svr\Data\Seeders;
 
+use Database\Seeders\DataAnimalsCodesSeeder;
+use Database\Seeders\DataApplicationsAnimalsSeeder;
+use Database\Seeders\DataApplicationsSeeder;
 use Database\Seeders\DataCompaniesLocationsSeeder;
 use Database\Seeders\DataCompaniesObjectsSeeder;
 use Database\Seeders\DataCompaniesSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DataSeeder extends Seeder
 {
     public function run()
     {
+        DB::statement("SET session_replication_role = 'replica';");
+
         (new DataCompaniesSeeder())->run();
         (new DataCompaniesLocationsSeeder())->run();
         (new DataCompaniesObjectsSeeder())->run();
+        (new DataAnimalsCodesSeeder())->run();
+        (new DataApplicationsSeeder())->run();
+        (new DataApplicationsAnimalsSeeder())->run();
+
+        DB::statement("SET session_replication_role = 'origin';");
     }
 }
